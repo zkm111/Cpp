daa5197de9c287c81ba7439dff0ab7f94c44561c
zkm111 <495347694@qq.com>
2019-08-06 16:46:52 +0800

    网络库

--- v2/Noncopyable.h
+++ v2/Noncopyable.h
@@ -1,25 +1,23 @@
  ///
  /// @file    Noncopyable.h
  /// @author  lemon(haohb13@gmail.com)
- /// @date    2019-06-08 10:55:15
+ /// @date    2019-05-08 10:49:34
  ///
- 
 
-#pragma once
+#ifndef __WD_NONCOPYABLE_H__
+#define __WD_NONCOPYABLE_H__
 
 namespace wd
 {
-
 class Noncopyable
 {
 protected:
 	Noncopyable(){}
-	~Noncopyable() {}
-
-	Noncopyable(const Noncopyable &) = delete;
+	~Noncopyable(){}
+	Noncopyable(const Noncopyable&) = delete;
 	Noncopyable & operator=(const Noncopyable &) = delete;
-
 };
-
-
 }//end of namespace wd
+
+#endif
+
